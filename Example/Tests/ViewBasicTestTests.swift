486a41c8cb1ba563bad14a03a0736a0fad4ae301
Pierre Felgines <pierre.felgines@fabernovel.com>
2019-04-03 17:20:49 +0200

    Create tests for recursive layout tests
    Change-Id: I60b276cc5a41eede8852e7c5c143295653a83e98

---
+++ Example/Tests/ViewBasicTestTests.swift
@@ -0,0 +1,60 @@
+//
+//  ViewBasicTestTests.swift
+//  AssertLayout_Tests
+//
+//  Created by Pierre Felgines on 01/04/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import XCTest
+@testable import AssertLayout
+
+class ViewBasicTestTests: XCTestCase {
+
+    func testNoProblems() {
+        let context = LayoutTestContext.base
+        let view = ViewFactory.createNoProblemsView()
+        XCTAssertNoThrow(try view.ad_runBasicRecursiveTests(using: context))
+    }
+
+    func testOverlapingViews() {
+        let context = LayoutTestContext.base
+        let view = ViewFactory.createOverlapingView()
+        XCTAssertThrowsError(try view.ad_runBasicRecursiveTests(using: context))
+    }
+
+    func testOverlapingViewsNoError() {
+        var context = LayoutTestContext.base
+        context.isViewOverlapTestEnabled = false
+        let view = ViewFactory.createOverlapingView()
+        XCTAssertNoThrow(try view.ad_runBasicRecursiveTests(using: context))
+    }
+
+    func testSubviewOutOfSuperview() {
+        let context = LayoutTestContext.base
+        let view = ViewFactory.createSubviewOutOfSuperviewView()
+        XCTAssertThrowsError(try view.ad_runBasicRecursiveTests(using: context))
+    }
+
+    func testSubviewOutOfSuperviewNoError() {
+        var context = LayoutTestContext.base
+        context.isViewWithinSuperviewBoundsTestEnabled = false
+        let view = ViewFactory.createSubviewOutOfSuperviewView()
+        XCTAssertNoThrow(try view.ad_runBasicRecursiveTests(using: context))
+    }
+
+    func testOverlapingErrorViews() {
+        let context = LayoutTestContext.base
+        let view = ViewFactory.createOverlapingErrorView()
+        XCTAssertThrowsError(try view.ad_runBasicRecursiveTests(using: context))
+    }
+
+    func testOverlapingErrorViewsNoError() {
+        var context = LayoutTestContext.base
+        context.viewClassNamesToSkip.insert("ErrorView")
+        let view = ViewFactory.createOverlapingErrorView()
+        XCTAssertNoThrow(try view.ad_runBasicRecursiveTests(using: context))
+    }
+}
---
+++ Example/Tests/ViewFactory.swift
@@ -0,0 +1,57 @@
+//
+//  ViewFactory.swift
+//  AssertLayout_Tests
+//
+//  Created by Pierre Felgines on 01/04/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ErrorView: UIView {}
+
+class ViewFactory {
+
+    static func createNoProblemsView() -> UIView {
+        let view1 = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 10.0, height: 10.0))
+        let view2 = UIView(frame: CGRect(x: 10.0, y: 0.0, width: 10.0, height: 10.0))
+        let superview = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 20.0, height: 10.0))
+        superview.addSubview(view1)
+        superview.addSubview(view2)
+        return superview
+    }
+
+    static func createOverlapingView() -> UIView {
+        let view1 = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 11.0, height: 5.0))
+        let view2 = UIView(frame: CGRect(x: 10.0, y: 4.0, width: 10.0, height: 5.0))
+        let superview = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 20.0, height: 10.0))
+        superview.addSubview(view1)
+        superview.addSubview(view2)
+        view1.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view2.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        return superview
+    }
+
+    static func createOverlapingErrorView() -> ErrorView {
+        let view1 = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 11.0, height: 5.0))
+        let view2 = UIView(frame: CGRect(x: 10.0, y: 4.0, width: 10.0, height: 5.0))
+        let superview = ErrorView(frame: CGRect(x: 0.0, y: 0.0, width: 20.0, height: 10.0))
+        superview.addSubview(view1)
+        superview.addSubview(view2)
+        view1.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view2.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        return superview
+    }
+
+    static func createSubviewOutOfSuperviewView() -> UIView {
+        let view1 = UIView(frame: CGRect(x: -1.0, y: 0.0, width: 10.0, height: 10.0))
+        let view2 = UIView(frame: CGRect(x: 10.0, y: 0.0, width: 10.0, height: 10.0))
+        let superview = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 20.0, height: 10.0))
+        superview.addSubview(view1)
+        superview.addSubview(view2)
+        view1.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view2.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        return superview
+    }
+}
