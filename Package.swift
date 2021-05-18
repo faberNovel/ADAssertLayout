5e542c04d685009a32027cd0746d30693e58f936
Emanuele Fumagalli <emanuelef@users.noreply.github.com>
2021-05-18 09:18:47 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+	name: "ADAssertLayout",
+	products: [
+		.library(
+			name: "ADAssertLayout",
+			targets: ["ADAssertLayout"]),
+	],
+	targets: [
+		.target(
+			name: "ADAssertLayout")
+	]
+)
